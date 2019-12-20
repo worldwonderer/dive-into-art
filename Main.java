8a491d2996e0999b047f436c9d9f310da9541cac
worldwonderer <xtchen.pitt@gmail.com>
2019-12-21 00:00:47 +0800

    Create read_class

---
+++ Main.java
@@ -0,0 +1,11 @@
+package io.github.worldwonderer;
+
+public class Main {
+
+    private static String a = "hello, world";
+
+    public static void main(String[] args) {
+	    // write your code here
+        System.out.println();
+    }
+}
